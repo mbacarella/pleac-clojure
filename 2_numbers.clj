4cfe20bc41b27e21d8ef555f00659a7560432b4c
Michael Bacarella <mbac@panix.com>
2011-09-12 22:14:56 -0400

    start section 2

---
+++ 2_numbers.clj
@@ -0,0 +1,52 @@
+;; 2. Numbers
+;; Checking Whether a String Is a Valid Number
+
+(import '(java.text NumberFormat ParseException)
+        '(java.util Locale))
+
+(def locale Locale/US)
+
+(defn nb [s]
+  (let [nf (NumberFormat/getInstance locale)]
+    (.parse nf s)))
+
+; user=> (nb "100")
+; 100
+; user=> (nb "not a number")
+; java.text.ParseException: Unparseable number: "not a number"
+
+; (def s1 100)
+; (def s1 "not a number")
+(try
+  (Integer/parseInt s1)
+  (catch NumberFormatException _ex
+    (.println System/out (str s1 " is not an integer"))))
+
+; (def s2 3.14)
+; (def s2 "foo")
+(try
+  (Float/parseFloat s2)
+  (catch NumberFormatException _ex
+    (.println System/out (str s2 " is not a float"))))
+
+(defn isNumeric [s]
+  (let [nf (NumberFormat/getInstance locale)]
+    (try
+      (do
+        (.parse nf s)
+        true)
+      (catch ParseException _ex false))))
+
+;; Comparing Floating-Point Numbers
+; (equal NUM1 NUM2 ACCURACY) returns true if NUM1 and NUM2 are
+; equal to ACCURACY number of decimal places
+(defn equal [num1 num2 accuracy]
+  (let [bigNum (fn [num]
+                 (.setScale
+                  (BigDecimal. num)
+                  accuracy
+                  BigDecimal/ROUND_DOWN))]
+    (= (.compareTo
+        (bigNum num1)
+        (bigNum num2)) 0)))
+
