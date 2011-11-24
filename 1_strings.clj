b2f30acaa9143d71ee64165b94fd539dc66a5f91
Andy Fingerhut <jafinger@cisco.com>
2011-11-23 23:14:50 -0800

    Use 2-argument subs when it matches the behavior of the 2-argument Perl substr.
    Replace format with printf, since corresponding Perl example does
    printing as side effect like Clojure printf.  Clojure format returns a
    string as a value like Perl sprintf, but without any I/O side effect.
    
    Correct example for exchanging values without temporary variables.

--- 1_strings.clj
+++ 1_strings.clj
@@ -20,6 +20,13 @@
 ;; or
 (def value (subs string offset))
 
+;; Clojure strings are immutable Java strings, so while you cannot
+;; modify an existing string, you can build a new one with part of it
+;; replaced by another.
+
+(def string (str (subs string 0 offset) newstring
+                 (subs string (+ offset count))))
+(def string (str (subs string 0 offset) newtail))
 
 ;; -----------------------------
 ;; get a 5-byte string, skip 3, then grab 2 8-byte strings, then the rest
@@ -58,26 +65,26 @@
 ;; "is"
 (def start (subs string 5 7))
 ;; "you have"
-(def rest (subs string 13 (count string)))
+(def rest (subs string 13))
 ;; "e" *)
 (def last (let [len (count string)]
-            (subs string (- len 1) len)))
+            (subs string (- len 1))))
 ;; "have"
 (def theend (let [len (count string)]
-              (subs string (- len 4) len)))
+              (subs string (- len 4))))
 ;; "you"
 (def piece (let [len (count string)]
              (subs string (- len 8) (- len 5))))
 
 ;; -----------------------------
 (def string "This is what you have")
-(format "%s" "string")
+(printf "%s" "string")
 
 ;; Change "is" to "wasn't"
 (def string (str
-             (subs string 0 4)
-             " wasn't "
-             (subs string 8)))
+             (subs string 0 5)
+             "wasn't"
+             (subs string 7)))
 ;; This wasn't what you have
 
 ;; This wasn't wonderous
@@ -85,7 +92,7 @@
      (str (subs string 0 (- (count string) 12)) "ondrous"))
 
 ;; delete first character
-(def string (subs string 1 (count string)))
+(def string (subs string 1))
 ;; his wasn't wondrous
 
 ;; delete last 10 characters
@@ -95,27 +102,64 @@
 
 ;; @@PLEAC@@_1.3 Establishing a Default Value
 
+;; While Perl treats undef, 0, and "" as false, Clojure treats the
+;; values false and nil as false, but 0 and "" as true.
+
 ;; -----------------------------
 ;; use b if b is true, else c
+;; Note that if b has never been defined or had a value bound to it,
+;; then unlike Perl this will give an error that the value is
+;; undefined.
 (def a (or b c))
 
 ;; re-define x with the value y, unless x is already true
 (def x (when-not x y))
 
 ;; use b if b is defined, otherwise c
+;; This correctly tests whether b is bound to a value or not, but
+;; if it is not, then it throws an exception because of the last
+;; occurrence of b not having a value.
 (def a (if (find (ns-interns *ns*) 'b) b c))
+;; This is closer:
+(def a (if (find (ns-interns *ns*) 'b) (eval 'b) c))
+
+;; But note that if b is only bound in a let, or as a function
+;; argument, but not at the top level with def or something similar,
+;; then this code will go with the value of c.
+(let [c "c-value"
+      b "b-value"]
+  (let [a (if (find (ns-interns *ns*) 'b) (eval 'b) c)]
+    (printf "a=%s" a)))
+;; a=c-value
+
 ;; -----------------------------
 (def foo (or bar "DEFAULT VALUE"))
 
+;; Clojure data structures are immutable.  The code below does not
+;; change the value of *command-line-args*, whereas Perl
+;; 'shift(@ARGV)' does modify @ARGV by removing its first element.
 (def dir
-  (if (> (count *command-line-args*) 1)
-    (subvec *command-line-args 1)
+  (if (>= (count *command-line-args*) 1)
+    (nth *command-line-args* 0)
     "/tmp"))
 
 ;; @@PLEAC@@_1.4 Exchanging Values Without Using Temporary Variables
 ;; -----------------------------
+
+;; This Clojure code does _not_ exchange values of var1 and var2
+;; without a temporary.  It binds var1 to the value of var2, then
+;; binds var2 to the new value of var1, so they both end up with the
+;; original value of var2.
+
 (let [var1 var2
       var2 var1])
+
+;; This will achieve the desired effect.  It creates a vector of the
+;; values of var2 and var1, then binds them using a technique called
+;; 'destructuring' to var1 and var2.
+
+(let [[var1 var2] [var2 var1]])
+
 
 ;; -----------------------------
 (def temp a)
@@ -124,9 +168,8 @@
 ;; -----------------------------
 (let [a "alpha"
       b "omega"]
-  (let [a b
-        b a]
-  ; the first shall be last -- and versa vice
+  (let [[a b] [b a]]
+    ;; the first shall be last -- and versa vice
     ))
 
 ;; -----------------------------
@@ -136,9 +179,7 @@
 ;; move beta to alpha
 ;; move production to beta
 ;; move alpha to production
-  (let [alpha beta
-        beta production
-        production alpha]
+  (let [[alpha beta production] [beta production alpha]]
 
     ))
 
